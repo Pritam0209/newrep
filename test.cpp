1426bec1e464ec1e6534584119ef29ad82040b81
Pritam0209 <pritamraj0209@gmail.com>
2020-05-12 00:09:31 +0530

    Create test.cpp
    new file

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void getComm(unsigned int len, char * src)
+{
+ unsigned int size;
+ size = len - 2;
+ char * comm = (char *) malloc(size + 1);
+ memcypy(comm, src, size);
+ return;
+}
+
+
+int main(){
+  cout<<"hell"<<endl;
+  return 0;
+}
